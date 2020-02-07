1536367bd03c09ddb6441b6f42472babb3d8d08f
yboodhan <yboodhan@gmail.com>
2020-02-07 14:20:37 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 }
