ad02e485ae067cba96d8b2c36770bec723dae5fd
yboodhan <yboodhan@gmail.com>
2020-02-09 14:28:57 -0800

    Completed projects page

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 const PortfolioCard = props => {
+    let features
+    if (props.features) {
+        features = "[String] features: [" + props.features + "]"
+    }
+
     return (
         <div>
             <a name={props.function}></a>
             <span style={props.typeStyle}>public void</span> {props.function}() {'{'}
             <br />
                 <div className="indented">
-                    <div>
-                        <div>
-                            {props.name}
-                        </div>
-                        <div>
-                            {props.skills}
-                        </div>
-                        <div>
-                            {props.description}
-                        </div>
-                    </div>
+                    String name: {"\"" + props.name + "\""}
                 </div>
+
+                <div className="indented">
+                    [String] skills: {"[" + props.skills + "]"}
+                </div>
+
+                <div className="indented">
+                    {features}
+                </div>
+                <br/>
+                <div className="indented">
+                    <a href={props.github} target="_blank">
+                        <FontAwesomeIcon icon={faGithub} />{' '}
+                        github();
+                    </a>
+                </div>
+
+                <div className="indented">
+                    <a href={props.deployed} target="_blank">
+                        <FontAwesomeIcon icon={faGlobe} />{' '}
+                        deployedSite();
+                    </a>
+                </div>
+                <br/>
+                <div className="indented text-indent">
+                    {props.description}
+                </div>
+
             {'}'}
         </div>
 
