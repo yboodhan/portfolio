435c797cf2d44d1d51cba0539e9ef796d85e56b8
yboodhan <yboodhan@gmail.com>
2020-02-09 18:16:19 -0800

    Change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-
+// import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './App.css';
 
 import Navigation from './components/Navigation';
