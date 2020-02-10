6494e4d22bf1d4b4abcaddd1dde92118a8b93b22
yboodhan <yboodhan@gmail.com>
2020-02-09 18:18:53 -0800

    Updates

---
+++ precache-manifest.695a0c21e49d66a8a19ff698b0aafe8d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53322e5744af7f1761dd18a926176c36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d09d58e7ab9c5546c9d",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1b46af488d61bc11112c",
+    "url": "/static/css/main.a2d313f1.chunk.css"
+  },
+  {
+    "revision": "2d09d58e7ab9c5546c9d",
+    "url": "/static/js/2.9d5024e5.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.9d5024e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b46af488d61bc11112c",
+    "url": "/static/js/main.61b752b1.chunk.js"
+  },
+  {
+    "revision": "fffbba1caad888857a56",
+    "url": "/static/js/runtime-main.ce901adf.js"
+  },
+  {
+    "revision": "52385c43047639259e33168aaad8ed06",
+    "url": "/static/media/Yashoma_Boodhan_Resume.52385c43.pdf"
+  }
+]);
